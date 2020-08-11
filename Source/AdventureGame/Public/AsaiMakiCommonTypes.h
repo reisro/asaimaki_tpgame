3d78426b4edc303ecd5850b6a9a3c7e0fec897b2
Rodrigo Reis <66571938+reisro@users.noreply.github.com>
2020-08-11 20:08:36 -0300

    Update AsaiMakiCommonTypes.h

--- Source/AdventureGame/Public/AsaiMakiCommonTypes.h
+++ Source/AdventureGame/Public/AsaiMakiCommonTypes.h
@@ -27,10 +27,28 @@ enum class CharacterAnimationState
 	Die
 };
 
+enum class ItemType
+{
+	Health,
+	CombatXP
+};
+
 USTRUCT(BlueprintType)
-struct ADVENTUREGAME_API FItem
+struct ADVENTUREGAME_API FAsaiMakiItem
 {
 	GENERATED_BODY()
+		
+	// The name of the item
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Items")
+	FString name;
+	
+	// Quick description of the item
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Items")
+	FString desc;
+	
+	// Type of the item
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Items")
+	ItemType itemType;
 };
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGameStarts);
